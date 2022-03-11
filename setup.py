f194bdd7b3514cd7db418332a4041d7aad951d91
Saul Pwanson <saul@voltrondata.com>
2022-03-10 18:52:31 -0800

    [setup] add python requirement

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
         name="jsom",
         version="2022.03.08",
+        python_requires='>=3.6',
+        py_modules=['jsom'],
         packages=["jsom"],
-        entry_points = {"console_scripts": ["jsom = jsom.__main__:main"]},
+        entry_points={"console_scripts": ["jsom=jsom.__main__:main"]},
     )
