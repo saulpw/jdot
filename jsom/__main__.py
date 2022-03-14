f08f2d38f6b5d835eb31262a735d5d92423b3fec
Saul Pwanson <saul@voltrondata.com>
2022-03-14 16:44:10 -0700

    [main] decode/encode into same JsonCoder to keep macros

--- jsom/__main__.py
+++ jsom/__main__.py
@@ -33,17 +33,21 @@ def read_arg(fn):
 def main():
     i = 1
     jsomargs = []
+    j = JsomCoder(indent=' ')
     while i < len(sys.argv):
         arg = sys.argv[i]
         i += 1
 
         if arg in ['-d', '--decode']:
             contents = read_arg(sys.argv[i])
-            print(jsom2json(contents))
+            d = j.decode(contents)
+            print(json.dumps(d, cls=JsonDefaultEncoder))
             i += 1
         elif arg in ['-e', '--encode']:
             contents = read_arg(sys.argv[i])
-            print(json2jsom(contents))
+            d = json.loads(contents)
+            s = j.encode(d)
+            print('\n'.join(line.rstrip() for line in s.splitlines()))
             i += 1
         else:
             jsomargs.append(arg)
