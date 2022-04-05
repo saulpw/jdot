91fa0b01cad633328ad0ebf9f87cdc2ee1d8a9de
Saul Pwanson <saul@voltrondata.com>
2022-04-04 23:40:59 -0700

    [cli] tojsom.py utility for yaml/json/jsom

---
+++ tojsom.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+import yaml
+
+import jsom
+
+j = jsom.JsomCoder()
+out = []
+
+for fn in sys.argv[1:]:
+    contents = open(fn).read()
+    if fn.endswith('yaml'):
+        d = yaml.load(contents, yaml.Loader)
+    elif fn.endswith('json'):
+        d = json.loads(contents)
+    elif fn.endswith('jsom'):
+        j.decode(contents)
+
+    if isinstance(d, list):
+        out.extend(d)
+    elif d:
+        out.append(d)
+
+print(j.encode(out))
