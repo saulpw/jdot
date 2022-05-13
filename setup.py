217dc1544c9f7c2f47132bcde09e5572ca83fa9f
anjakefala <anja@voltrondata.com>
2022-05-13 11:19:23 -0700

    [build] add pypi description metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(
         name="jdot",
         version="0.5",
+        description='JSON with minimal punctuation, plus Macros',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        author='Saul Pwanson',
+        url='https://github.com/saulpw/jdot',
         python_requires='>=3.7',
         py_modules=['jdot'],
         packages=["jdot"],
